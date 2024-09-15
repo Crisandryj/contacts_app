93ad2c704cb42308c29ec2c41fcf49fdecf5b948
Crisandry Javier <crisandryj@gmail.com>
2024-09-15 10:52:28 -0400

    Feat: Add contact ID

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,17 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 
@@ -18,4 +23,3 @@ createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
   </StrictMode>
 );
-g;
